50d0ffba3301ea01e8d2cd55dbf60414c20a8e46
Fatima Ibrahim <2421235@iub.edu.bd>
2026-01-11 21:50:35 +0600

    new

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:weather_app/weather_screen.dart';
 
 void main() {
   runApp(const MyApp());
@@ -14,6 +15,8 @@ class MyApp extends StatefulWidget {
 class _MyAppState extends State<MyApp> {
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp();
+    return MaterialApp(
+      home: const WeatherScreen(),
+    );
   }
 }
